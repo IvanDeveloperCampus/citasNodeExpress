a59e244f16dfa74fed769a22ad276096e49704f1
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-07-12 08:38:04 -0500

    feat: :sparkles: Quinto EndPoint
    Encontrar todos los pacientes que tienen citas con un médico específico (por ejemplo, el médico con **med_nroMatriculaProsional 1**)

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -3,7 +3,8 @@ import getConnection from '../db/database.js'
 
 const storageUsuario=Router();
 
-storageUsuario.get("/get", async(req, res)=>{
+storageUsuario
+.get("/getUsuarios", async(req, res)=>{
     try {
         const connection= await getConnection();
         const [rows, fields] = await connection.execute('SELECT * FROM usuario order by usu_nombre ASC');
@@ -11,6 +12,17 @@ storageUsuario.get("/get", async(req, res)=>{
       } catch (error) {
         res.status(400).send(error.message);
       } 
+
+})
+.get("/usuariosMedicoEspecifico/:nroMatriculaMedico", async(req, res)=>{
+  try {
+      let id=req.params.nroMatriculaMedico;
+      const connection= await getConnection();
+      const [rows, fields] = await connection.execute('select DISTINCT usuario.usu_nombre from usuario INNER JOIN cita ON usuario.usu_id=cita.cit_datosUsuario INNER JOIN medico ON cita.cit_medico=medico.med_nroMatriculaProfesional WHERE medico.med_nroMatriculaProfesional=?;', [id]);
+      res.send(rows);
+    } catch (error) {
+      res.status(400).send(error.message);
+    } 
 
 })
 
