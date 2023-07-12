478bf2e125cfe9d34cdd45b9cb7a2809bff4022e
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-07-12 08:52:55 -0500

    fix: :bug: Correcion tercer endpoint, era consultorios y yo liste fue citas
    Obtener las consultorías para un paciente específico (por ejemplo, paciente **con usu_id 1**)

--- src/routes/cita.routes.js
+++ src/routes/cita.routes.js
@@ -26,17 +26,7 @@ storageCita
   
   })
 
-  .get("/citasUsuarioEspecifico", async(req, res)=>{
-    try {
-        const connection= await getConnection();
-        let id=req.body.usu_id;
-        const [rows, fields] = await connection.execute('SELECT * from cita INNER JOIN usuario ON cita.cit_datosUsuario=?;',[id]);
-        res.send(rows);
-      } catch (error) {
-        res.status(400).send(error.message);
-      } 
-  
-  })
+
 
   .get("/citasFechaEspecifica", async(req, res)=>{
     try {
