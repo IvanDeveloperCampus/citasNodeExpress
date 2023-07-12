69097a301b6a781f2dd76fb99d3371e77dee405f
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-07-12 08:42:23 -0500

    feat: :sparkles: Sexto EndPoint
    Obtener las consultorías para un paciente específico (por ejemplo, paciente **con usu_id 1**)

--- src/routes/cita.routes.js
+++ src/routes/cita.routes.js
@@ -17,8 +17,20 @@ storageCita
 .get("/proximaCita", async(req, res)=>{
     try {
         const connection= await getConnection();
-        let id=req.body.user_id;
+        let id=req.body.usu_id;
         const [rows, fields] = await connection.execute('SELECT cit_fecha FROm cita WHERE cit_datosUsuario=? AND cit_fecha>CURDATE() ORDER BY cit_fecha ASC LIMIT 1;',[id]);
+        res.send(rows);
+      } catch (error) {
+        res.status(400).send(error.message);
+      } 
+  
+  })
+
+  .get("/citasUsuarioEspecifico", async(req, res)=>{
+    try {
+        const connection= await getConnection();
+        let id=req.body.usu_id;
+        const [rows, fields] = await connection.execute('SELECT * from cita INNER JOIN usuario ON cita.cit_datosUsuario=?;',[id]);
         res.send(rows);
       } catch (error) {
         res.status(400).send(error.message);
