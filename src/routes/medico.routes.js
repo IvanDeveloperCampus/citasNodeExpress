252915e58b87086ff1bb5384ca71dbc8859fbe38
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-07-12 08:09:59 -0500

    feat: :sparkles: Tercer EndPoint
    Obtener todos los médicos de una especialidad específica (por ejemplo, **'Cardiología'**):

---
+++ src/routes/medico.routes.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import getConnection from '../db/database.js'
+
+const storageMedico=Router();
+
+storageMedico
+.get("/getMedicos", async(req, res)=>{
+  try {
+      let especialidad=req.query.especialidad;
+      const connection= await getConnection();
+      const [rows, fields] = await connection.execute('SELECT medico.med_nombreCompleto FROM `medico` INNER JOIN especialidad ON med_especialidad=especialidad.esp_id WHERE especialidad.esp_nombre=?;', [especialidad]);
+      res.send(rows);
+    } catch (error) {
+      res.status(400).send(error.message);
+    } 
+
+})
+
+
+export default storageMedico;
